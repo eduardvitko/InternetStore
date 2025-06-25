870c39ad349820d107f62c593c0a2faa76c615d6
User <eduardvitko@gmail.com>
2025-06-25 23:07:50 +0300

    add and setting AdminController method getAllCategories

--- src/main/java/com/example/InternetStore/controller/AdminController.java
+++ src/main/java/com/example/InternetStore/controller/AdminController.java
@@ -1,12 +1,15 @@
 package com.example.InternetStore.controller;
 
+import com.example.InternetStore.dto.CategoryDto;
 import com.example.InternetStore.dto.UserDto;
 import com.example.InternetStore.model.User;
+import com.example.InternetStore.services.CategoryServise;
 import com.example.InternetStore.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -16,13 +19,15 @@ public class AdminController {
 
 
     private final UserService userService;
+    private final CategoryServise categoryServise;
 
-    public AdminController(UserService userService) {
+    public AdminController(UserService userService, CategoryServise categoryServise) {
 
         this.userService = userService;
+        this.categoryServise = categoryServise;
     }
 
-    @PostMapping("/users")
+    @PostMapping("/create")
     public User createUser(@RequestBody User user) {
         return userService.create(user);
     }
@@ -31,6 +36,7 @@ public class AdminController {
     public Set<UserDto> getAllUsers() {
         return userService.getAllUsers();
     }
+
     @DeleteMapping("/delete/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> deleteUser(@PathVariable int id) {
@@ -40,11 +46,17 @@ public class AdminController {
         userService.deleteById(id);
         return ResponseEntity.ok().build();
     }
+
     @PutMapping("/update/{id}")
     public ResponseEntity<?> updateUser(@PathVariable Integer id, @RequestBody User updatedUser) {
         return userService.updateUser(id, updatedUser)
                 .map(user -> ResponseEntity.ok().body("User updated"))
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/all/categories")
+    public List<CategoryDto> getAllCategories() {
+        return categoryServise.getAllCategories();
     }
 }
 
