e243d4583cfd9bfe7d16381d3f97c5d5775bf95d
User <eduardvitko@gmail.com>
2025-07-28 11:31:02 +0300

    delete constructor

--- src/main/java/com/example/InternetStore/services/PaymentService.java
+++ src/main/java/com/example/InternetStore/services/PaymentService.java
@@ -22,10 +22,7 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final OrderRepository orderRepository;
 
-    public PaymentService(PaymentRepository paymentRepository, OrderRepository orderRepository) {
-        this.paymentRepository = paymentRepository;
-        this.orderRepository = orderRepository;
-    }
+
 
     // Створити новий платіж
     @Transactional
